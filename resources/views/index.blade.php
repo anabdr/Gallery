813f8bab39d8a6af75eab2f4fdce9e22d50be2e7
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-05-14 14:10:06 +0200

    change index css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
                         html += '<p>' + data.inventoryId + '</p>';
                         html += '<p>' + data.year + '</p>';
                         html += '<p>' + data.dimensions + '</p>';
-                        html += '<p>' + data.price + ' ' + data.currency + ' ' + data.secondPrice + '</p>';
+                        html += '<p class="price">' + data.price + ' ' + data.currency + ' <span></span> ' + data.secondPrice + '</p>';
                         html += '<div class="formActions">'
                         html += '<a href="'+baseUrl+'/edit/'+data.inventoryId+'" id="edit"><svg class="iconEdit" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg></a>';
                         html += '<button id="exportPDF">export PDF</button>';
